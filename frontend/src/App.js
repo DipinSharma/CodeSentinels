aa1651e63d8d1644f3ff61cc67db1f53f03bdd09
SatyaX100 <satyajeetverma70@gmail.com>
2024-08-09 08:53:07 +0530

    VideoChat page and Home Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
+import './index.css'; // or './tailwind.css'
 
-
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+import Home from "./pages/Home";
+import VideoChat from "./pages/VideoChat";
 function App() {
-  return (<></>);
+  return (<>
+    
+      <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/videoChat" element={<VideoChat></VideoChat>}></Route>
+      </Routes>
+    
+  </>);
 }
 
 export default App;
---
+++ frontend/src/context/stateReducers.js
@@ -0,0 +1,16 @@
+export const initialState={
+
+
+
+}
+
+
+export const reducer=(state,action) =>{
+    switch (action.type){
+        // define youractions here
+
+
+        default:
+            return state
+    }
+}
